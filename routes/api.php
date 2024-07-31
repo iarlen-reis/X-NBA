4f266de13006992a3f3aab141336b9a04f973593
Iarlen <iarlensr@hotmail.com>
2024-07-31 01:02:41 -0300

    feat: add api routes to index and show Team.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\HelloWord;
+use App\Http\Controllers\Api\TeamsController;
 use Illuminate\Support\Facades\Route;
 
 // hello world route for testing swagger
 Route::get('/hello', [HelloWord::class, 'index']);
+
+Route::get('/teams', [TeamsController::class, 'index']);
+Route::get('/teams/{id}', [TeamsController::class, 'show']);
