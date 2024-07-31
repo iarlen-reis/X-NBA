99f79c7de24f34cabd2e937d52bdf439a2875226
Iarlen <iarlensr@hotmail.com>
2024-07-31 01:02:19 -0300

    feat: create Team migrations and Team seeder.

---
+++ database/migrations/2024_07_31_012855_create_teams_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('stadium');
+            $table->string('city');
+            $table->string('country');
+            $table->string('coach');
+            $table->string('league');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
