d74e4c9d87138bcbbd2b0817f2226fb91e149dbb
Iarlen <iarlensr@hotmail.com>
2024-07-31 00:59:26 -0300

    feat: create Team model and Team factory.

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->streetName(),
+            'slug' => $this->faker->slug(),
+            'stadium' => $this->faker->word(),
+            'city' => $this->faker->city(),
+            'country' => $this->faker->country(),
+            'coach' => $this->faker->name(),
+            'league' => $this->faker->randomElement(['NBA', 'WNBA']),
+        ];
+    }
+}
