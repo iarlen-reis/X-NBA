920f24e64015c9c7a357f280a99d492c3617d315
Iarlen <iarlensr@hotmail.com>
2024-08-02 00:49:24 -0300

    feat: add apiResource to Team and api docs.

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,4 @@ use Illuminate\Support\Facades\Route;
 // hello world route for testing swagger
 Route::get('/hello', [HelloWord::class, 'index']);
 
-Route::get('/teams', [TeamsController::class, 'index']);
-Route::get('/teams/{id}', [TeamsController::class, 'show']);
+Route::apiResource('teams', TeamsController::class);
