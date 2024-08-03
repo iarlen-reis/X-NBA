f733d3b7e703e62f2c8a800c1bfbe57e58f951dd
Iarlen <iarlensr@hotmail.com>
2024-08-03 00:17:42 -0300

    feat: create Player repository and interface.

---
+++ app/Repositories/Implementations/PlayerRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories\Implementations;
+
+use App\Models\Player;
+use App\Repositories\Contracts\PlayerRepositoryInterface;
+
+class PlayerRepository implements PlayerRepositoryInterface
+{
+    public function index()
+    {
+        return Player::all();
+    }
+
+    public function show($id)
+    {
+        return Player::findOrFail($id)->load('team');
+    }
+
+    public function store(array $data)
+    {
+        return Player::create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $player = Player::findOrFail($id);
+
+        $player->update($data);
+
+        return $player;
+    }
+
+    public function destroy($id)
+    {
+        $player = Player::findOrFail($id);
+
+        $player->update(['active' => !$player->active]);
+
+        return $player;
+    }
+}
