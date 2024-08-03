6b06b14f146e035b53c866ebae30570936691bad
Iarlen <iarlensr@hotmail.com>
2024-08-03 00:17:04 -0300

    feat: create Player model, factory and migrations.

---
+++ database/factories/PlayerFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Team;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Player>
+ */
+class PlayerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'age' => $this->faker->numberBetween(18, 40),
+            'height' => $this->faker->numberBetween(60, 200),
+            'weight' => $this->faker->numberBetween(200, 800),
+            'position' => $this->faker->randomElement(['F', 'C', 'G', 'PG', 'SG']),
+            'league' => $this->faker->randomElement(['NBA', 'WNBA']),
+            'active' => $this->faker->boolean(chanceOfGettingTrue: 100),
+            'team_id' => Team::factory(),
+        ];
+    }
+}
---
+++ database/migrations/2024_08_02_213630_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->integer('age');
+            $table->integer('height');
+            $table->integer('weight');
+            $table->string('position');
+            $table->string('league');
+            $table->uuid('team_id');
+            $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
