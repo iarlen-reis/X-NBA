7f27845cc67f3d870137e1c904b6239aa1831897
Iarlen <iarlensr@hotmail.com>
2024-08-04 14:13:31 -0300

    feat: remove Hello Word controller, api route and tests.

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AveragesController;
-use App\Http\Controllers\Api\HelloWord;
 use App\Http\Controllers\Api\PlayersController;
 use App\Http\Controllers\Api\TeamsController;
 use Illuminate\Support\Facades\Route;
-
-// hello world route for testing swagger
-Route::get('/hello', [HelloWord::class, 'index']);
 
 Route::apiResource('teams', TeamsController::class);
 Route::apiResource('players', PlayersController::class);
