4f0b88ff8bab06af676e06476cc3f8da024e06bc
Iarlen <iarlensr@hotmail.com>
2024-08-06 21:32:21 -0300

    feat: add Matche api routes.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AveragesController;
+use App\Http\Controllers\Api\MatchesController;
 use App\Http\Controllers\Api\PlayersController;
 use App\Http\Controllers\Api\TeamsController;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('teams', TeamsController::class);
 Route::apiResource('players', PlayersController::class);
 Route::apiResource('averages', AveragesController::class);
+Route::apiResource('matches', MatchesController::class);
