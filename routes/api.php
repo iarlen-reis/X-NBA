b582af90ec14d28e66eb262919a45daf7f719f83
Iarlen <iarlensr@hotmail.com>
2024-09-05 02:20:41 -0300

    feat: add match-teams and stats routes.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Api\AveragesController;
 use App\Http\Controllers\Api\MatchesController;
+use App\Http\Controllers\Api\MatchesTeamController;
 use App\Http\Controllers\Api\PlayersController;
+use App\Http\Controllers\Api\StatsController;
 use App\Http\Controllers\Api\TeamsController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +12,6 @@ Route::apiResource('teams', TeamsController::class);
 Route::apiResource('players', PlayersController::class);
 Route::apiResource('averages', AveragesController::class);
 Route::apiResource('matches', MatchesController::class);
+
+Route::apiResource('matche-teams', MatchesTeamController::class);
+Route::apiResource('stats', StatsController::class);
