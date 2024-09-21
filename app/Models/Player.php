2a91615f6774cff167c39d32aadd0608605612bc
Iarlen <iarlensr@hotmail.com>
2024-09-20 23:15:38 -0300

    feat: add stats relationship to the player.

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Player extends Model
@@ -22,5 +23,10 @@ class Player extends Model
     public function average(): HasOne
     {
         return $this->hasOne(Average::class);
+    }
+
+    public function stats(): HasMany
+    {
+        return $this->hasMany(Stats::class);
     }
 }
